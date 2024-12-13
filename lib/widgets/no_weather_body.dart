9adaaf8ae233f41d281dcf1b65c050d0b619c26d
aheit <aeita4@yahoo.com>
2024-12-13 02:54:58 +0200

    finish ui

--- lib/widgets/no_weather_body.dart
+++ lib/widgets/no_weather_body.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/ui/city_search_view.dart';
 
 class NoWeatherBody extends StatelessWidget {
-  const NoWeatherBody({
-    super.key,
-  });
+  const NoWeatherBody({super.key,});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return Center(child: GestureDetector(onTap: () {
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+        return const CitySearchView();
+      }));
+    },
+      child: const Column(mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            "there is no weather 😔 start",
-            style: TextStyle(fontSize: 28),
-          ),
-          Text(
-            'searching now 🔍',
-            style: TextStyle(fontSize: 28),
-          )
-        ],
-      ),
-    );
+          Text("there is no weather 😔 start", style: TextStyle(fontSize: 28),),
+          Text('searching now 🔍', style: TextStyle(fontSize: 28),)
+        ],),),);
   }
 }
