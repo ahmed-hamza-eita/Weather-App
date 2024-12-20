1a94752d277c12b4fbb45605c1cf057ef7a10970
aheit <aeita4@yahoo.com>
2024-12-20 02:21:33 +0200

    create states

---
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_state.dart';
+
+class GetWeatherCubit extends Cubit<WeatherState> {
+  GetWeatherCubit(super.initialState);
+
+}
