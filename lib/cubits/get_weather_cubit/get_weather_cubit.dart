46787fc3256312da72b3ec9aa5bac17c4febb36e
aheit <aeita4@yahoo.com>
2024-12-20 15:59:42 +0200

    integrate cubit

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -5,7 +5,7 @@ import 'package:weather_app/models/weather_data.dart';
 import 'package:weather_app/services/weather_service.dart';
 
 class GetWeatherCubit extends Cubit<WeatherState> {
-  GetWeatherCubit() : super(NoWeatherState());
+  GetWeatherCubit() : super(WeatherInitialState());
 
   getCurrentWeather({required String cityName}) async {
     try {
--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_state.dart';
 import 'package:weather_app/widgets/no_weather_body.dart';
+import 'package:weather_app/widgets/weather_info_body.dart';
 
 import 'city_search_view.dart';
 
@@ -33,7 +37,18 @@ class HomeView extends StatelessWidget {
           ),
         ),
       ),
-      body: NoWeatherBody(),
+      body:
+          BlocBuilder<GetWeatherCubit, WeatherState>(builder: (context, state) {
+        if (state is WeatherInitialState) {
+          return const NoWeatherBody();
+        } else if (state is WeatherLoadedState) {
+          return const WeatherInfoBody(
+            weatherData: "",
+          );
+        } else {
+          const Text("Error");
+        }
+      }),
     );
   }
 }
